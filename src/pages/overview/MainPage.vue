9304ef0a0f62100070307acef1ce631330b42116
Mauricio <mauriciorojasanchez@gmail.com>
2025-05-07 02:08:52 -0500

    chore: leave a basic usage of shared components.

--- src/pages/overview/MainPage.vue
+++ src/pages/overview/MainPage.vue
@@ -1,16 +1,23 @@
 <script>
 import TopBarComponent from "../../shared/components/TopBarComponent.vue";
 import SearchBar from "../../shared/components/SearchBarComponent.vue";
+import ButtonComponent from "../../shared/components/ButtonComponent.vue";
 
 export default {
     name: "MainPage",
-  components: {SearchBar, TopBarComponent}
+  components: {ButtonComponent, SearchBar, TopBarComponent},
+  methods: {
+      handleButton() {
+        console.log("Hello!");
+      }
+  }
 }
 </script>
 
 <template>
   <TopBarComponent />
   <SearchBar />
+  <ButtonComponent text="Hola" state="primary" @click="handleButton"/>
 </template>
 
 <style scoped>
