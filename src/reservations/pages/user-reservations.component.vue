012c9d178409e02637def0ba8bb07d024088171e
Arian Rodriguez <arianmrv12@gmail.com>
2025-05-08 17:11:35 -0500

    add(user-reservations): added breadcrumb path for guest reservations

--- src/reservations/pages/user-reservations.component.vue
+++ src/reservations/pages/user-reservations.component.vue
@@ -1,11 +1,41 @@
 <script>
+import BreadCrumb from "../../shared/components/breadcrumb.component.vue";
+import userMock from "../../mocks/iam/user-profile-account.json";
+
 export default {
-    name: "MyReservationsPage"
+    name: "MyReservationsPage",
+    components: {
+        BreadCrumb
+    },
+    data() {
+      return {
+        userData: null,
+        breadcrumbPath: [
+          { name: "Account", route: "" },
+          { name: "My Reservations", route: "" },
+        ]
+      }
+    },
+    mounted() {
+      this.fetchUserData();
+    },
+    methods: {
+      fetchUserData() {
+        setTimeout(() => {
+          this.userData = userMock;
+          console.log("User data fetched:", this.userData);
+
+          this.breadcrumbPath[0].route = `/home/profile/${this.userData.id}`;
+          this.breadcrumbPath[1].route = `/home/profile/${this.userData.id}/reservations`;
+        }, 300);
+      }
+    }
 }
 </script>
 
 <template>
- <div>
+  <div>
+    <BreadCrumb :path="breadcrumbPath" />
     <h1>My reservations Page</h1>
   </div>
 </template>
