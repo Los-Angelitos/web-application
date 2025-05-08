655eedbfd2791f7962c3fc9f921b67e6009c743c
Arian Rodriguez <arianmrv12@gmail.com>
2025-05-08 15:04:15 -0500

    add(user-preferences): opening modal for room card lost

--- src/iam/pages/guest-preferences.component.vue
+++ src/iam/pages/guest-preferences.component.vue
@@ -1,6 +1,8 @@
 <script>
 import AccountDetailFormEdit from '../components/account-detail-form-edit.component.vue';
 import AccountInfoOverview from '../components/account-info-overview.component.vue';
+import ModalComponent from '../../shared/components/modal.component.vue';
+import ButtonComponent from '../../shared/components/button.component.vue';
 import userMock from '../../mocks/iam/user-profile-account.json';
 
 export default {
@@ -8,10 +10,13 @@ export default {
     components: {
         AccountDetailFormEdit,
         AccountInfoOverview,
+        ModalComponent,
+        ButtonComponent
     },
     data() {
       return {
         userData: null,
+        showModal: false,
       };
     },
     mounted() {
@@ -31,23 +36,76 @@ export default {
       editField(field, value) {
         console.log("Editing field:", field, "Current value:", value);
       },
+      handleModal(action = 'toggle') {
+        if (action === 'open') {
+          this.showModal = true;
+        } else if (action === 'close') {
+          this.showModal = false;
+        } else {
+          this.showModal = !this.showModal;
+        }
+      }
+
     }
 }
 </script>
 
 <template>
+  
+  <ModalComponent v-model="showModal" title="Request Room Card" :showCloseButton="true" :closeOnOverlayClick="true" width="350px" height="auto" class="modal">
+    <template #image>
+      <img src="../../assets/iam/card_id_room.svg" alt="Card ID" class="icon-card-id" />
+    </template>
+    <template #header>
+      <h1>Room Card</h1>
+    </template>
+    <template #body>
+      <p>I request the cancellation of my previous room card, requiring a new one.</p>
+    </template>
+    <template #footer>
+      <ButtonComponent :state="'basic'" :text="'Cancel'" :onClick="() => handleModal('close')" :width="'50%'"/>
+      <ButtonComponent :state="'primary'" :text="'Request'" :onClick="() => handleModal('close')" :width="'50%'"/>
+    </template>
+  </ModalComponent>
+
  <div class="user-preferences-container">
   <div class="account-path">
     <span class="account-path-text">Account > Personal Information > My Preferences</span>
   </div>
 
   <div class="user-preferences-content">
-    <AccountInfoOverview v-if="userData" :username="userData.name" :userType="userData.type" />
+    <AccountInfoOverview v-if="userData" :username="userData.name" :userType="userData.type" class="user-overview" />
+
+    <div class="form-edit">
+
+      <AccountDetailFormEdit v-if="userData" :userData="userData.preferences" :notShowFields="['id', 'type', 'created_at', 'updated_at', 'image']" @edit="editField" @save="saveField" />
+
+      <div class="account-detail-item">
+        <div class="item-fields">
+          <p class="account-detail-item__field">Can't you find your room card? </p>
+          <p class="account-detail-item__field-value">
+            Request the cancellation of your room card and obtain a new one.
+          </p>
+        </div>
+
+        <div class="item-edit" @click="() => handleModal('open')">
+          <img src="/icon_open.svg" alt="Request Icon" class="edit-icon" />
+          <p class="account-detail-item__edit">Request</p>
+        </div>
+      </div>
+    </div>
+
+    
   </div>
  </div>
+
 </template>
 
 <style scoped>
+::v-deep(.modal-image) {
+  background-color: transparent;
+}
+
 .user-preferences-container {
   margin: 2rem;
 }
@@ -64,4 +122,70 @@ export default {
   font-size: .8rem;
 }
 
+.user-overview {
+  width: 100%;
+  max-width: 550px;
+  margin-top: 2rem;
+}
+
+.form-edit {
+  width: 100%;
+  max-width: 550px;
+  margin-top: 2rem;
+}
+
+
+.account-detail-item {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem 0;
+  border-bottom: 1px solid var(--gray-extra-light-color);
+  width: 100%;
+  
+}
+
+.item-fields{
+  display: flex;
+  flex-direction: column;
+  padding: 1rem 0;
+}
+
+.account-detail-item__field {
+  font-size: 1.2rem;
+}
+
+.account-detail-item__field-value {
+  font-size: .9rem;
+  color: var(--gray-light-color);
+}
+
+.item-edit {
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+}
+
+.edit-icon {
+  width: .8rem;
+  height: .8rem;
+  margin-right: .5rem;
+}
+
+.account-detail-item__edit {
+  font-size: .9rem;
+  color: #000;
+}
+
+.modal {
+  text-align: center;
+}
+
+.modal h1 {
+  font-size: 1.3rem;
+}
+
+::v-deep(.modal-footer) {
+  justify-content: space-around;
+}
 </style>
