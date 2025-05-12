6ea779ac6cb90ff9640f709e9ce0579521083216
Mauricio <mauriciorojasanchez@gmail.com>
2025-05-11 23:57:22 -0500

    chore(auth): added http common.

---
+++ src/shared/services/http-common.js
@@ -0,0 +1,27 @@
+﻿/**
+ * Axios default configuration
+ */
+
+import axios from "axios";
+// import {authenticationInterceptor} from "../../iam/services/authentication.interceptor.js";
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+/**
+ * Axios instance
+ * @summary http axios instance with default configuration
+ *
+ * @type {AxiosInstance}
+ */
+const http = axios.create({
+    baseURL: API_BASE_URL,
+});
+
+// Set default headers
+http.defaults.headers.common['Content-Type'] = 'application/json';
+http.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+
+// Add authentication interceptor
+// http.interceptors.request.use(authenticationInterceptor);
+
+export default http;
