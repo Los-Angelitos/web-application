2690cd9c48e5c37ae156485a49492b482ec69943
Salvlormonch1 <salvlormonch@gmail.com>
2025-05-12 19:29:56 -0500

    fix(shared): corrected baseURL.

--- src/shared/services/hotels-api.service.js
+++ src/shared/services/hotels-api.service.js
@@ -3,6 +3,6 @@ import http from "../../shared/services/http-common.js";
 export class HotelsApiService {
 
     getHotelsById(hotelId) {
-        return http.get(`/hotels/${hotelId}`);
+        return http.get(`/v1/hotels/${hotelId}`);
     }
 }
